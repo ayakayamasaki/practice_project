1d15d5075d00a18912f77464a50d08ca9e6cd3e5
yamasaki ayaka <mpmjampwpmpwdmgm@gmail.com>
2023-11-07 11:56:56 +0900

    java 1-6 commit

---
+++ src/main/java/practice/PracticeForEach.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class PracticeForEach {
+
+	public static void main(String[] args) {
+		String name[] = { "田原", "福尾", "倉成", "濱野", "前村", "重永", "田中", "山田", "山口", "高田" };
+
+		for (String order : name) {
+			System.out.println(order);
+		}
+	}
+
+}
---
+++ src/main/java/practice/PracticeWhile.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class PracticeWhile {
+
+	public static void main(String[] args) {
+		int num = 101;
+
+		while (num > 0) {
+			--num;
+
+			System.out.println(num);
+		}
+	}
+}
